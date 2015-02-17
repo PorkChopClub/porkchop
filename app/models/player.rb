6714613e49b1c4bfe66f132263d97d4b8ddd96b6
Jared Norman <jared@freerunningtechnologies.com>
2015-02-16 23:25:57 -0800

    Add matches "association" to player
    Change-Id: I02189ecc70fa027cb86e0c167b0b53de80c9bfa4

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,10 @@
 class Player < ActiveRecord::Base
   has_many :points, foreign_key: 'victor_id'
 
+  def matches
+    Match.where "matches.home_player_id = :id OR matches.away_player_id = :id",
+      id: id
+  end
+
   validates :name, presence: true
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe Player, type: :model do
   it "is valid with a name" do
     expect(described_class.new(name: 'Jeff').valid?).to be true
   end
+
+  describe "#matches" do
+    subject { player.matches }
+
+    let!(:away_match) { FactoryGirl.create :match, away_player: player }
+    let!(:home_match) { FactoryGirl.create :match, home_player: player }
+    let(:player) { FactoryGirl.create :player }
+
+    it "includes both home and away matches" do
+      expect(subject).to match_array [home_match, away_match]
+    end
+  end
 end
