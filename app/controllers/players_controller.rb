29ac91b480315228cd585044df6ec3f5a2956d7f
Jared Norman <jared@freerunningtechnologies.com>
2015-02-17 21:26:04 -0800

    Show record on player page
    Change-Id: I63f7927671f695895d42274445fae1b5d4169986

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,6 @@
 class PlayersController < ApplicationController
   def show
     @player = Player.find(params[:id])
+    @stats = Stats::Personal.new(@player)
   end
 end
--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -5,6 +5,14 @@ RSpec.describe PlayersController, type: :controller do
     subject { get :show, id: player.to_param }
 
     let(:player) { FactoryGirl.create :player }
+    let(:stats) { instance_double Stats::Personal }
+
+    before do
+      expect(Stats::Personal).
+        to receive(:new).
+        with(player).
+        and_return(stats)
+    end
 
     specify { expect(subject.status).to eq 200 }
     it { is_expected.to render_template :show }
@@ -12,6 +20,11 @@ RSpec.describe PlayersController, type: :controller do
     it "assigns the player as @player" do
       subject
       expect(assigns(:player)).to eq player
+    end
+
+    it "assigns the personal stats as @stats" do
+      subject
+      expect(assigns(:stats)).to eq stats
     end
   end
 end
