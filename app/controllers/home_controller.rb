a382f93c1e86d2036f0a9be5aa0c441d7eee2f6c
Jared Norman <jared@freerunningtechnologies.com>
2015-02-18 23:52:20 -0800

    Show recent matches on homepage
    Change-Id: Id6276ca725ade7e8d468b01b4feb2dc4c71a5ce1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,5 @@
 class HomeController < ApplicationController
+  def index
+    @recent_matches = Match.finalized.order(finalized_at: :desc).limit(10)
+  end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,7 +4,15 @@ RSpec.describe HomeController, type: :controller do
   describe "GET index" do
     subject { get :index }
 
+    let!(:recent_match) { FactoryGirl.create :complete_match }
+    let!(:ongoing_match) { FactoryGirl.create :match }
+
     specify { expect(subject.status).to eq 200 }
     it { is_expected.to render_template :index }
+
+    it "assigns the recent matches as @recent_matches" do
+      subject
+      expect(assigns(:recent_matches)).to match_array [recent_match]
+    end
   end
 end
