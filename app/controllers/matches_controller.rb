1133e9ce986fd97f4b1219ed0fef484a8dcf372e
Jared Norman <jared@freerunningtechnologies.com>
2015-02-27 13:31:54 -0800

    Exclude finalized matches from match index
    Change-Id: I23dd7c44f38856a870a2327912c09a8d1b6a8ff4

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,7 +3,7 @@ class MatchesController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @matches = @matches.order(finalized_at: :desc)
+    @matches = @matches.finalized.order(finalized_at: :desc)
   end
 
   def new
