3657d34e79a277afbe89b6662634df3a01bc52cb
Adam Mueller <adam@freerunningtech.com>
2015-03-04 18:48:55 -0800

    Don't verify CSRF token for table API.
    Change-Id: Ibe8bcf09af520c9eea686f90614cdb3e4a3f0bdc

--- app/controllers/api/table_controller.rb
+++ app/controllers/api/table_controller.rb
@@ -1,5 +1,6 @@
 class Api::TableController < ApplicationController
   skip_authorization_check
+  skip_before_action :verify_authenticity_token
 
   before_action :check_table_token
   before_action :match
