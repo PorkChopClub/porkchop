80cd1c5f2799be3acd75b922d5bb8be3b5b13ad4
Gray Gilmore <graygilmore@gmail.com>
2015-03-11 13:30:52 -0700

    Add leaderboard to the home page
    Change-Id: I7410519d78630a5ed5bc4efc82467a39a05675c1

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe HomeController, type: :controller do
 
     let!(:recent_match) { FactoryGirl.create :complete_match }
     let!(:ongoing_match) { FactoryGirl.create :match }
+    let!(:gray) { FactoryGirl.create :player, name: "Gray", elo: 1000 }
+    let(:match_history) { FactoryGirl.create_list :complete_match, 20, home_player: gray }
 
     specify { expect(subject.status).to eq 200 }
     it { is_expected.to render_template :index }
@@ -13,6 +15,12 @@ RSpec.describe HomeController, type: :controller do
     it "assigns the recent matches as @recent_matches" do
       subject
       expect(assigns(:recent_matches)).to match_array [recent_match]
+    end
+
+    it "assigns eligible players as @ranked_players" do
+      match_history
+      subject
+      expect(assigns(:ranked_players)).to match_array [gray]
     end
   end
 end
