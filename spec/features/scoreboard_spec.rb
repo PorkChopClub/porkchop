8fa209add00e16775b73f73e0f52a6695d2391b4
John Hawthorn <john.hawthorn@gmail.com>
2015-03-23 22:55:19 -0700

    Add scoreboard spec
    Change-Id: Ib512e9abf0bd7f9f4bdb440f3a87d33d637c3706

---
+++ spec/features/scoreboard_spec.rb
@@ -0,0 +1,53 @@
+require 'feature_helper'
+
+RSpec.describe "scoreboard page" do
+  let!(:home) { FactoryGirl.create :player, name: "Candice Bergen" }
+  let!(:away) { FactoryGirl.create :player, name: "Shirley Schmidt" }
+
+  scenario "recording a normal game" do
+    visit '/scoreboard'
+
+    # Blank page
+
+    create_match
+
+    expect(page).to have_content(home.name)
+    expect(page).to have_content(away.name)
+    expect(page).to have_content("This is the first match between these players")
+    expect(page).to have_content("Select first service")
+
+    table.home_button
+    expect(page).to have_css('.scoreboard-home-player.has-service')
+    expect(page).to have_no_content("Select first service")
+
+    table.away_button
+    table.home_button
+    table.home_button
+
+    within '.scoreboard-home-player-score' do
+      expect(page).to have_content '2'
+    end
+    within '.scoreboard-away-player-score' do
+      expect(page).to have_content '1'
+    end
+    expect(page).to have_css('.scoreboard-away-player.has-service')
+
+    8.times { table.home_button }
+
+    expect(page).to have_content('Game point')
+    table.home_button
+
+    expect(page).to have_content('Press to finalize')
+    table.home_button
+
+    # blank page again
+  end
+
+  def create_match
+    @match = FactoryGirl.create :match, :at_start, home_player: home, away_player: away
+  end
+
+  def table
+    PingPong::TableControls.new(PingPong::Match.new(@match))
+  end
+end
