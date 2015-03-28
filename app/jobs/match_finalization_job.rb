996d9938fdb589d547a1b57e3a02390020bdba4a
John Hawthorn <john@freerunningtechnologies.com>
2015-03-27 23:39:20 -0700

    Extract send_notification! into class
    Change-Id: I5be4b815555a80b730498c78b6b8e59fe9b4af7d

--- app/jobs/match_finalization_job.rb
+++ app/jobs/match_finalization_job.rb
@@ -13,17 +13,7 @@ class MatchFinalizationJob < ActiveJob::Base
   attr_reader :match
 
   def send_notification!
-    return unless ENV["SLACK_WEBHOOK_URL"]
-    notifier.ping(
-      "#{victor_name} defeated #{loser_name}",
-      attachments: [{
-        fallback: "#{home_player_name} #{match.home_score} - #{match.away_score} #{away_player_name}",
-        fields: [
-          { title: home_player_name, value: match.home_score },
-          { title: away_player_name, value: match.away_score }
-        ]
-      }]
-    )
+    SlackNotification.new(match).deliver
   end
 
   def adjust_elo!
@@ -37,29 +27,5 @@ class MatchFinalizationJob < ActiveJob::Base
     [match.home_player, match.away_player].each do |player|
       player.all_achievements.select(&:earned?).each(&:adjust_rank!)
     end
-  end
-
-  def victor_name
-    match.victor.name
-  end
-
-  def loser_name
-    match.loser.name
-  end
-
-  def home_player_name
-    match.home_player.name
-  end
-
-  def away_player_name
-    match.away_player.name
-  end
-
-  def notifier
-    @notifier ||= Slack::Notifier.new(
-      ENV["SLACK_WEBHOOK_URL"],
-      username: "PorkChop",
-      icon_emoji: ":trophy:"
-    )
   end
 end
---
+++ app/models/slack_notification.rb
@@ -0,0 +1,56 @@
+class SlackNotification
+  def initialize(match)
+    @match = match
+  end
+
+  def deliver
+    return unless webhook_url
+    notifier.ping title, attachments: attachments
+  end
+
+  private
+
+  attr_reader :match
+
+  def title
+    "#{victor_name} defeated #{loser_name}"
+  end
+
+  def attachments
+    [{
+      fallback: "#{home_player_name} #{match.home_score} - #{match.away_score} #{away_player_name}",
+      fields: [
+        { title: home_player_name, value: match.home_score },
+        { title: away_player_name, value: match.away_score }
+      ]
+    }]
+  end
+
+  def victor_name
+    match.victor.name
+  end
+
+  def loser_name
+    match.loser.name
+  end
+
+  def home_player_name
+    match.home_player.name
+  end
+
+  def away_player_name
+    match.away_player.name
+  end
+
+  def webhook_url
+    ENV["SLACK_WEBHOOK_URL"]
+  end
+
+  def notifier
+    @notifier ||= Slack::Notifier.new(
+      webhook_url,
+      username: "PorkChop",
+      icon_emoji: ":trophy:"
+    )
+  end
+end
