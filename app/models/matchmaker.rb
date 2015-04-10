f6e57e0711d2abe074a15d47359c0ae054557a24
John Hawthorn <john.hawthorn@gmail.com>
2015-04-09 20:35:13 -0700

    Use Time.at(0) instead of Time.new(0)
    Time.at(0)
    => 1969-12-31 16:00:00 -0800
    
    Time.new(0)
    => 0000-01-01 00:00:00 -0812
    
    Time.at(0) is far faster to calculate than Time.new(0) and for our
    purposes it makes no difference.
    
    Change-Id: Ie8f4f70b13acd968ff7ec4fb84af12273e5aa613

--- app/models/matchmaker.rb
+++ app/models/matchmaker.rb
@@ -1,6 +1,8 @@
 class Matchmaker
   attr_reader :players
 
+  EPOCH = Time.at(0)
+
   def initialize(players)
     @players = players.to_a
   end
@@ -10,7 +12,7 @@ class Matchmaker
   end
 
   def choose
-    home_player = players.min_by{ |player| player.last_played_at || Time.new(0) }
+    home_player = players.min_by{ |player| player.last_played_at || EPOCH }
     away_player = (players - [home_player]).sample
     [home_player, away_player]
   end
