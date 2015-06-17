23783a31490a24dcb6bce1505f522ac4d4eaad84
Adam Mueller <adam@freerunningtech.com>
2015-06-16 20:48:35 -0700

    Add ongoing scope and validation.
    Number of matchup should be even so that we can guarantee that the
    players play both sides of the table.
    
    Change-Id: Ic416c6ae3109e70873d6071a8046140c199e0643

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,11 +1,13 @@
 class Season < ActiveRecord::Base
-  validates :games_per_matchup, numericality: { greater_than: 0 }
+  validates :games_per_matchup, numericality: { greater_than: 0, even: true }
 
   has_many :season_memberships
   has_many :players, through: :season_memberships
 
   has_many :season_matches
   has_many :matches, through: :season_matches
+
+  scope :ongiong, -> { where finalized_at: nil }
 
   def finalize!
     touch(:finalized_at) unless finalized_at
--- spec/factories/season.rb
+++ spec/factories/season.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :season do
-    games_per_matchup 1
+    games_per_matchup 2
 
     trait :finalized do
       sequence(:finalized_at) { |n| n.minutes.ago }
