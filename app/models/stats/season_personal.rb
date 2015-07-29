6b9e373d5cd673d53f31046998d6ab1998475b94
Adam Mueller <adam@freerunningtech.com>
2015-07-28 17:22:24 -0700

    Add point differential to season stats
    Change-Id: I1cdb846a2b27f1f68def0646ce7928394f7b1bdf

--- app/models/stats/season_personal.rb
+++ app/models/stats/season_personal.rb
@@ -1,8 +1,14 @@
 class Stats::SeasonPersonal
+  include Comparable
+
   attr_reader :player
 
   def initialize(season, player)
     @season, @player = season, player
+  end
+
+  def <=>(other)
+    [win_ratio, point_differential] <=> [other.win_ratio, other.point_differential]
   end
 
   def matches_played
@@ -24,11 +30,31 @@ class Stats::SeasonPersonal
     format_ratio (matches_won.to_f / matches_played.to_f)
   end
 
+  def points_for
+    points.select do |match|
+      match.victor_id == player.id
+    end.size
+  end
+
+  def points_against
+    points.select do |match|
+      match.victor_id != player.id
+    end.size
+  end
+
+  def point_differential
+    points_for - points_against
+  end
+
   private
   attr_reader :season
 
   def matches
     @matches ||= season.matches.finalized.where("home_player_id = :player OR away_player_id = :player", player: player)
+  end
+
+  def points
+    @points ||= matches.map(&:points).flatten
   end
 
   def format_ratio(ratio)
--- spec/models/stats/season_personal_spec.rb
+++ spec/models/stats/season_personal_spec.rb
@@ -11,6 +11,26 @@ RSpec.describe Stats::SeasonPersonal do
     FactoryGirl.create :complete_match, away_player: player, season: season
   end
 
+  describe "#<=>" do
+    let!(:other) { described_class.new(season, other_player) }
+    let(:other_player) { FactoryGirl.create :player }
+    before do
+      FactoryGirl.create :complete_match, home_player: other_player, season: season
+    end
+    context "when they have the same win ratio" do
+      subject { stats <=> other }
+      it { is_expected.to eq -1 }
+    end
+    context "when the win ratio is the same" do
+      before do
+        FactoryGirl.create :complete_match, home_player: other_player, season: season
+        FactoryGirl.create :complete_match, away_player: other_player, season: season, away_score: 2
+      end
+      subject { stats <=> other }
+      it { is_expected.to eq 1 }
+    end
+  end
+
   describe "#matches_played" do
     subject { stats.matches_played }
     it { is_expected.to eq 3 }
@@ -38,5 +58,20 @@ RSpec.describe Stats::SeasonPersonal do
       let(:stats) { described_class.new(season, other) }
       it { is_expected.to eq ".000" }
     end
+  end
+
+  describe "#points_for" do
+    subject { stats.points_for }
+    it { is_expected.to eq 28 }
+  end
+
+  describe "#points_against" do
+    subject { stats.points_against }
+    it { is_expected.to eq 23 }
+  end
+
+  describe "#point_differential" do
+    subject { stats.point_differential }
+    it { is_expected.to eq 5 }
   end
 end
