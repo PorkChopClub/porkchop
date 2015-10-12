706be281ca3cf1297ed67d130277fe8d9b60c84d
Andrew Stewart <andrew@stwrt.ca>
2015-10-11 17:30:23 -0700

    Switch to Webpack for SASS builds
    Now down to a single asset watcher for development mode.
    
    Adds appropriate loaders to webpack to handle stylesheets, and a text
    extraction plugin to generate a .css file.
    
    Additionally, uses Webpack's de-dupe and uglifyjs plugins in production
    mode to minify generated assets.

--- chop/webpack.config.js
+++ chop/webpack.config.js
@@ -1,24 +1,54 @@
-var path = require('path');
-var config = module.exports = {};
+"use strict";
 
-config.context = __dirname;
+var ExtractTextPlugin = require("extract-text-webpack-plugin"),
+    webpack = require("webpack");
 
-config.entry = { scoreboard: './web/static/js/scoreboard.js' };
+var path = require("path");
 
-config.output = {
-  path: path.join(__dirname, "priv/static/js"),
-  filename: "[name].js"
-}
+function join(dest) { return path.resolve(__dirname, dest); }
+function web(dest) { return join("web/static/" + dest); }
 
-config.module = {
-  loaders: [
-    { test: /\.js$/,
-      exclude: /node_modules/,
-      loader: "babel-loader" }
+var sassOpts = [
+  "?includePaths[]=", join("node_modules"),
+  "&includePaths[]=", join("bower_components"),
+
+  // fuck u bourbon
+  "&includePaths[]=", join("node_modules/bourbon/app/assets/stylesheets")
+].join("");
+
+var config = module.exports = {
+  entry: [
+    web("css/pork.scss"),
+    web("js/scoreboard.js")
+  ],
+
+  output: {
+    path: join("priv/static"),
+    filename: "js/[name].js"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel?optional[]=runtime"
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("style", "css!sass" + sassOpts)
+      }
+    ]
+  },
+
+  plugins: [
+    new ExtractTextPlugin("css/pork.css")
   ]
-}
-
-config.resolve = {
-  // So we can do `require('./utils')` instead of `require('./utils.js')`
-  extensions: ['', '.js']
 };
+
+if (process.env.NODE_ENV === "production") {
+  config.plugins.push(
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true })
+  );
+}
