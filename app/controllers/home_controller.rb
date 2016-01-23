db6ee121c279e6e8f62f4f28b833528bb59c0995
Jared Norman <jared@creepywizard.com>
2016-01-22 23:26:50 -0800

    Clean things up maybe?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,35 +10,39 @@ class HomeController < ApplicationController
   end
 
   def load_ranked_players
-    @ranked_players = Player.all.
-                      select do |p|
-                        p.matches.finalized.count >= 20 &&
-                        p.matches.finalized.where("finalized_at > ?", 14.days.ago).count > 4
-                      end.
-                      sort_by(&:elo).
-                      reverse
+    @ranked_players =
+      Player.all.
+      select do |p|
+        p.matches.finalized.count >= 20 &&
+        p.matches.finalized.where("finalized_at > ?", 14.days.ago).count > 4
+      end.
+      sort_by(&:elo).
+      reverse
   end
 
   def load_elo_data
     @elo_range = (30.days.ago.to_date..Date.current)
 
-    @all_elo_ratings = EloRating.where(created_at: @elo_range).group_by do |rating|
-      [rating.player_id, rating.created_at.to_date]
-    end.map do |(player_id, date), ratings|
-      rating = ratings.last
-      [[player_id, date], rating.rating]
-    end.to_h
+    @all_elo_ratings =
+      EloRating.
+      where(created_at: @elo_range).
+      group_by { |rating| [rating.player_id, rating.created_at.to_date] }.
+      map { |(player_id, date), ratings| [[player_id, date], ratings.last.rating] }.
+      to_h
 
-    @elo_data = @ranked_players.map do |player|
-      previous_rating = player.elo_on(@elo_range.first)
-      [
-        player.name,
-        @elo_range.map do |d|
-          rating = @all_elo_ratings[[player.id, d]]
-          previous_rating = rating if rating
-          [d, rating || previous_rating]
-        end.to_h
-      ]
-    end.to_h
+    @elo_data =
+      @ranked_players.
+      map do |player|
+        previous_rating = player.elo_on(@elo_range.first)
+        [
+          player.name,
+          @elo_range.map do |d|
+            rating = @all_elo_ratings[[player.id, d]]
+            previous_rating = rating if rating
+            [d, rating || previous_rating]
+          end.to_h
+        ]
+      end.
+      to_h
   end
 end
