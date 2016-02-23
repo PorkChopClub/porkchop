41e007f55e6268ca93bb7f2f2e990bf8b1947e8f
Jared Norman <jared@creepywizard.com>
2016-02-22 21:17:52 -0800

    Make the toggle happen

--- app/webpack/components/header.js
+++ app/webpack/components/header.js
@@ -1,4 +1,11 @@
 import $ from 'jquery';
 
 module.exports = ($element) => {
+  let isMenuOpen =
+    $element
+      .asEventStream('click', '.main-header-menu-button')
+      .scan(false, (value) => !value);
+
+  isMenuOpen
+    .assign($('.main-header-menu-button'), 'toggleClass', 'active');
 };
