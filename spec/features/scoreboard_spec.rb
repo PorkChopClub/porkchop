ab1cb2d260a5a574c5aa6977be3197924ebd8ee2
Jared Norman <jared@creepywizard.com>
2016-02-25 23:16:08 -0800

    Fix spec

--- spec/features/scoreboard_spec.rb
+++ spec/features/scoreboard_spec.rb
@@ -17,12 +17,12 @@ RSpec.describe "scoreboard page" do
     expect(page).to have_content(home.name)
     expect(page).to have_content(away.name)
     expect(page).to have_content("01:30")
-    expect(page).to have_content("Select first service")
+    expect(page).to have_content("SELECT FIRST SERVICE")
 
     table.home_button
     expect(page).to have_content("This is the first match between these players")
     expect(page).to have_css('.scoreboard-home-player.has-service')
-    expect(page).to have_no_content("Select first service")
+    expect(page).to have_no_content("SELECT FIRST SERVICE")
 
     table.away_button
     table.home_button
@@ -41,7 +41,7 @@ RSpec.describe "scoreboard page" do
     expect(page).to have_content('Game point')
     table.home_button
 
-    expect(page).to have_content('Press to finalize')
+    expect(page).to have_content('PRESS TO FINALIZE')
     table.home_button
 
     # blank page again
