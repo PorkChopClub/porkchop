89a11dcab645e902dba6c928253bf2eebb4d99d3
John Hawthorn <john.hawthorn@gmail.com>
2016-05-15 12:36:18 -0700

    Move player activations to webpack

--- app/webpack/default.js
+++ app/webpack/default.js
@@ -2,6 +2,7 @@ import './shared';
 import header from './components/header';
 import './components/player_elo_chart';
 import './components/homepage_elo_chart';
+import './components/player_activations';
 
 $(() => header($('.main-navigation')));
 
