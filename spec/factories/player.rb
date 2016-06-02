6621a1f7d32a55ad9d41c24741892b6ec924ff90
Jared Norman <jared@creepywizard.com>
2016-06-01 20:37:51 -0700

    Add basic player ability

--- spec/factories/player.rb
+++ spec/factories/player.rb
@@ -4,5 +4,13 @@ FactoryGirl.define do
     password "password"
     sequence(:name) { |n| "Candice Bergen ##{n}" }
     avatar_url "http://bit.ly/15E65sT"
+
+    trait :confirmed do
+      confirmed_at { 1.hour.ago }
+    end
+
+    factory :admin_player, traits: [:confirmed] do
+      sequence(:email) { |n| "cbergen#{n}@stembolt.com" }
+    end
   end
 end
---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+require "cancan/matchers"
+
+RSpec.describe Ability do
+  subject { Ability.new(player) }
+
+  context "without a player" do
+    let(:player) { nil }
+
+    it { is_expected.to be_able_to(:read, :all) }
+    it { is_expected.not_to be_able_to(:create, :all) }
+    it { is_expected.not_to be_able_to(:update, :all) }
+    it { is_expected.not_to be_able_to(:destroy, :all) }
+  end
+
+  context "when the player isn't an admin" do
+    let(:player) { create :player }
+
+    it { is_expected.to be_able_to(:read, :all) }
+    it { is_expected.not_to be_able_to(:create, :all) }
+    it { is_expected.not_to be_able_to(:update, :all) }
+    it { is_expected.not_to be_able_to(:destroy, :all) }
+  end
+
+  context "when the player is an admin" do
+    let(:player) { create :admin_player }
+
+    it { is_expected.to be_able_to(:read, :all) }
+    it { is_expected.to be_able_to(:create, :all) }
+    it { is_expected.to be_able_to(:update, :all) }
+    it { is_expected.to be_able_to(:destroy, :all) }
+  end
+end
