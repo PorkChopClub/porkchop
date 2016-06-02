6789b0e5bd57beeed5affc95ac46b4545adac0c5
Jared Norman <jared@creepywizard.com>
2016-06-01 21:52:13 -0700

    Fix ScoreboardsController

--- spec/controllers/scoreboards_controller_spec.rb
+++ spec/controllers/scoreboards_controller_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe ScoreboardsController, type: :controller do
 
   describe "GET edit" do
     subject { get :edit, session: { write_access: true } }
+    before { sign_in create(:admin_player) }
     let(:match) { FactoryGirl.create :match }
     it { is_expected.to have_http_status :success }
   end
