138e1b80440cecbe6c909a0fa24c84d16a39336c
Jared Norman <jared@creepywizard.com>
2016-07-10 00:07:56 -0700

    Configure Preact as our React wannabe, and test

--- app/webpack/default.js
+++ app/webpack/default.js
@@ -1,4 +1,5 @@
 import $ from 'jquery'
+import { h, render } from 'preact'
 
 import './shared'
 import header from './components/header'
@@ -7,5 +8,13 @@ import './components/homepage_elo_chart'
 import './components/player_activations'
 import './components/match_controls'
 
-$(() => header($('.main-navigation')))
+$(() => {
+  header($('.main-navigation'))
+  render((
+    <div id="foo">
+      <span>Hello, world!</span>
+      <button onClick={ e => alert("hi!") }>Click Me</button>
+    </div>
+  ), document.body);
+});
 
--- webpack.config.js
+++ webpack.config.js
@@ -25,10 +25,7 @@ module.exports = function(options) {
     loaders: [
       { test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          presets: ['es2015-webpack']
-        }
+        loader: "babel-loader"
       }
     ]
   }
