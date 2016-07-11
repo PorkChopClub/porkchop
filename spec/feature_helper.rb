32d13621e2183dccb5298e3ecb43e5eecaedba72
Jared Norman <jared@creepywizard.com>
2016-07-10 23:55:10 -0700

    Use prebuilt phantomjs with cabybara

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -3,9 +3,13 @@ require 'capybara/rails'
 require 'capybara/poltergeist'
 
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new app,
-                                    window_size: [1920, 1080],
-                                    js_errors: true
+  Capybara::Poltergeist::Driver.new(
+    app,
+    phantomjs: File.expand_path("../../node_modules/phantomjs-prebuilt/bin/phantomjs", __FILE__),
+    phantomjs_options: ['--load-images=no'],
+    window_size: [1920, 1080],
+    js_errors: true
+  )
 end
 Capybara.javascript_driver = :poltergeist
 Capybara.default_driver = :poltergeist
