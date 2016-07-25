dfee5ec3cc590a1edd6377119383046a81272b65
Jared Norman <jared@creepywizard.com>
2016-07-24 23:35:43 -0700

    Stop using prebuilt phantomjs
    It's installing 1.9.8 for some reason... which is terrible.

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -5,7 +5,6 @@ require 'capybara/poltergeist'
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(
     app,
-    phantomjs: File.expand_path("../../node_modules/phantomjs-prebuilt/bin/phantomjs", __FILE__),
     phantomjs_options: ['--load-images=no'],
     window_size: [1920, 1080],
     js_errors: true
