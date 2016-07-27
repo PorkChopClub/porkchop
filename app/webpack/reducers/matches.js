7972747798963e6a76366d97371ca5f3440da711
Jared Norman <jared@creepywizard.com>
2016-07-27 00:06:59 -0700

    Simplify match state structure

--- app/webpack/reducers/matches.js
+++ app/webpack/reducers/matches.js
@@ -1,13 +1,12 @@
 import { handleActions } from 'redux-actions';
 
 const defaultState = {
-  matches: {}
 }
 
 export default handleActions({
   MATCH_UPDATE: (state, { payload }) => {
-    const matches = { ...state.matches }
-    matches[payload.id] = payload
-    return { ...state, matches: matches }
+    const newState = { ...state }
+    newState[payload.id] = payload
+    return newState
   }
 }, defaultState)
