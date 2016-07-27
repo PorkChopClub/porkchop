ca82ad6d0da72bc0b41fc2f1f2c7a779de12405b
Jared Norman <jared@creepywizard.com>
2016-07-26 18:48:21 -0700

    Add placeholder scoreboard component

---
+++ app/webpack/components/Scoreboard.js
@@ -0,0 +1,11 @@
+import { h, Component } from 'preact'
+
+class Scoreboard extends Component {
+  render() {
+    return (
+      <div>Scoreboard!</div>
+    )
+  }
+}
+
+export default Scoreboard
--- app/webpack/scoreboard.js
+++ app/webpack/scoreboard.js
@@ -1,1 +1,11 @@
 import './shared'
+
+import { h, render } from 'preact'
+import Scoreboard from './components/Scoreboard'
+
+const scoreboard = document.getElementById('scoreboard')
+
+render(
+  <Scoreboard />,
+  scoreboard
+)
