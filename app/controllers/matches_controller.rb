09bbb3a1933088fc359be27927f3139378bd2a70
John Hawthorn <john@stembolt.com>
2016-08-26 16:34:35 -0700

    Add pagination to matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,6 +2,6 @@ class MatchesController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @matches = @matches.finalized.order(finalized_at: :desc)
+    @matches = @matches.finalized.order(finalized_at: :desc).page(params[:page]).per(50)
   end
 end
