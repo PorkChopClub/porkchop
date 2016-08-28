cf22e1941066efccee7a86cc9a433a2f27d740c8
John Hawthorn <john.hawthorn@gmail.com>
2016-08-27 22:43:45 -0700

    Upgrade chart.js

--- app/webpack/components/player_elo_chart.js
+++ app/webpack/components/player_elo_chart.js
@@ -10,23 +10,34 @@ $(() => {
     labels: $.map(rawData, () => ''),
     datasets: [
       {
-        fillColor: 'rgba(153,134,142,0.2)',
-        strokeColor: 'rgba(153,134,142,1)',
-        pointColor: 'rgba(153,134,142,1)',
-        pointStrokeColor: '#fff',
-        pointHighlightFill: '#fff',
-        pointHighlightStroke: 'rgba(153,134,142,1)',
+        label: 'ELO',
+
+        backgroundColor: 'rgba(153,134,142,0.2)',
+        borderColor: 'rgba(153,134,142,1)',
         data: rawData,
+
+        pointRadius: 0,
+        pointHoverRadius: 10,
+        pointHitRadius: 10,
       },
     ],
   }
   const options = {
-    showTooltips: false,
-    scaleShowHorizontalLines: false,
-    scaleShowVerticalLines: false,
-    pointDot: false,
-    responsive: true,
+    legend: {
+      display: false,
+    },
+    scales: {
+      xAxes: [{
+        display: false,
+      }],
+    },
   }
   const ctx = canvas.get(0).getContext('2d')
-  new Chart(ctx).Line(chartData, options)
+
+  /* eslint-disable no-new */
+  new Chart(ctx, {
+    type: 'line',
+    data: chartData,
+    options,
+  })
 })
