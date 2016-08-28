53aa6a8fbce96a73bc63f1f3cc827c654b686fdb
Jared Norman <jared@creepywizard.com>
2016-08-28 11:59:15 -0700

    Support longer names a little better

--- app/webpack/components/ScoreboardPlayer.js
+++ app/webpack/components/ScoreboardPlayer.js
@@ -2,14 +2,19 @@ import { h, Component } from 'preact'
 import classNames from 'classnames'
 
 export default ({ player, score, hasService }) => {
+  console.log(player)
+  const displayName = player.nickname || player.name
+  const longName = displayName.length >= 24
+
   const classes = classNames({
     "scoreboard-player": true,
-    "has-service": hasService === false
+    "has-service": hasService === false,
+    "long-name": longName
   })
 
   return (
     <div className={classes}>
-      <div className="scoreboard-player-name">{player.nickname || player.name}</div>
+      <div className="scoreboard-player-name">{displayName}</div>
       <div>{score}</div>
     </div>
   )
