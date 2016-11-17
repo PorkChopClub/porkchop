798e9ef59abb1aa0c40291c0078fb28b144d4359
Jared Norman <jared@creepywizard.com>
2016-11-16 22:22:51 -0800

    Send new portrait urls to dashboard

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,7 +1,16 @@
 class PlayerSerializer < ActiveModel::Serializer
   attributes :id,
              :nickname,
-             :name
+             :name,
+             :portrait_url
 
   attribute :legacy_avatar_url, key: :avatar_url
+
+  def portrait_url
+    if object.profile_picture?
+      object.profile_picture.url
+    else
+      "/avatars/default.png"
+    end
+  end
 end
