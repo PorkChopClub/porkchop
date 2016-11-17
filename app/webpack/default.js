0e95524d3ba2affc853d5bf3b1833778f2e83642
Jared Norman <jared@creepywizard.com>
2016-11-16 22:54:44 -0800

    Remove old header and make footer sticky

--- app/webpack/default.js
+++ app/webpack/default.js
@@ -1,18 +1,4 @@
-import { h, render } from 'preact'
-
 import './components/player_elo_chart'
 import './components/homepage_elo_chart'
 import './components/player_activations'
 import './components/match_controls'
-
-import Header from './components/Header'
-
-const header = document.getElementById('main-navigation')
-
-render(
-  <Header
-    loggedIn={header.dataset.loggedIn === 'true'}
-    showControls={header.dataset.showControls === 'true'}
-  />,
-  header
-)
