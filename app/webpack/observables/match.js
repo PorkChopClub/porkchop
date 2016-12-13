6670b14d67366e432af71906e0a45c62e3d564dc
Jared Norman <jared@creepywizard.com>
2016-12-12 19:44:22 -0800

    Skip duplicates for performance/correctness

--- app/webpack/observables/match.js
+++ app/webpack/observables/match.js
@@ -1,11 +1,11 @@
-export const homeScore = (match) => match.map('.home_score')
-export const awayScore = (match) => match.map('.away_score')
+export const homeScore = (match) => match.map('.home_score').skipDuplicates()
+export const awayScore = (match) => match.map('.away_score').skipDuplicates()
 
-export const homeName = (match) => match.map('.home_player.name')
-export const awayName = (match) => match.map('.away_player.name')
+export const homeName = (match) => match.map('.home_player.name').skipDuplicates()
+export const awayName = (match) => match.map('.away_player.name').skipDuplicates()
 
-export const homePortrait = (match) => match.map('.home_player.portrait_url')
-export const awayPortrait = (match) => match.map('.away_player.portrait_url')
+export const homePortrait = (match) => match.map('.home_player.portrait_url').skipDuplicates()
+export const awayPortrait = (match) => match.map('.away_player.portrait_url').skipDuplicates()
 
-export const homeService = (match) => match.map('.home_service')
-export const awayService = (match) => match.map('.away_service')
+export const homeService = (match) => match.map('.home_service').skipDuplicates()
+export const awayService = (match) => match.map('.away_service').skipDuplicates()
