faebff8429a67e4e0279858a7347626e2f49a5a4
Jared Norman <jared@creepywizard.com>
2016-12-12 22:11:59 -0800

    Show correct views

--- app/webpack/scoreboard.js
+++ app/webpack/scoreboard.js
@@ -9,6 +9,23 @@ const match = ongoingMatch(tableId)
 
 $(document.body).html(scoreboardTemplate())
 
-match.log()
+match
+  .map((match) => {
+    if (match) {
+      return 'ongoing-match'
+    } else {
+      return 'no-match'
+    }
+  })
+  .skipDuplicates()
+  .scan(
+    { currentState: 'no-match' },
+    ({ currentState: previousState }, currentState) => ({ currentState, previousState })
+  )
+  .onValue(({ currentState, previousState }) => {
+    $(`.scoreboard-page.scoreboard-${previousState}`).removeClass('active');
+
+    $(`.scoreboard-page.scoreboard-${currentState}`).addClass('active');
+  })
 
 ongoingMatchComponent($('.scoreboard-ongoing-match'), match)
