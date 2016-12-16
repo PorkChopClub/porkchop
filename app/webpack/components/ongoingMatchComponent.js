9422438d65be13b202077c8b9287f5a62041ab28
Jared Norman <jared@creepywizard.com>
2016-12-15 23:19:08 -0800

    Animate score change

--- app/webpack/components/ongoingMatchComponent.js
+++ app/webpack/components/ongoingMatchComponent.js
@@ -22,19 +22,31 @@ const ongoingMatchComponent = ($el, match) => {
       })
   }
 
+  const refreshScore = (player) =>
+    (score) => {
+      const $score = $el.find(`.ongoing-match-player.${player} .score`)
+      $score.addClass('tiny')
+
+      setTimeout(() => {
+        $score
+          .text(score)
+          .removeClass('tiny')
+      }, 150)
+    }
+
   homeName(match).assign($el.find('.ongoing-match-player.home .name'), 'text')
-  homeScore(match).assign($el.find('.ongoing-match-player.home .score'), 'text')
   homePortrait(match)
     .map((url) => `url(${url})`)
     .assign($el.find('.ongoing-match-player.home .portrait'), 'css', 'background-image')
   setPlayerServiceState('home', homeServiceState(match))
+  homeScore(match).onValue(refreshScore('home'))
 
   awayName(match).assign($el.find('.ongoing-match-player.away .name'), 'text')
-  awayScore(match).assign($el.find('.ongoing-match-player.away .score'), 'text')
   awayPortrait(match)
     .map((url) => `url(${url})`)
     .assign($el.find('.ongoing-match-player.away .portrait'), 'css', 'background-image')
   setPlayerServiceState('away', awayServiceState(match))
+  awayScore(match).onValue(refreshScore('away'))
 }
 
 export default ongoingMatchComponent
