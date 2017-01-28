0a9d5dd183de31a5b2d58a8d29c4a378db616515
Jared Norman <jared@creepywizard.com>
2017-01-27 22:47:12 -0800

    Conver original template to JSX

--- app/webpack/scoreboard.js
+++ app/webpack/scoreboard.js
@@ -3,6 +3,38 @@ import './banner'
 import { render } from 'react-dom'
 
 render(
-  <h1>Hello, world!</h1>,
+  <div className="scoreboard">
+    <div className="scoreboard-ongoing-match scoreboard-page">
+      <div className="ongoing-match-players">
+        <div className="ongoing-match-player home">
+          <div className="portrait"></div>
+          <div className="name"></div>
+          <div className="score"></div>
+        </div>
+
+        <div className="ongoing-match-player away">
+          <div className="portrait"></div>
+          <div className="name"></div>
+          <div className="score"></div>
+        </div>
+      </div>
+
+      <div className="ongoing-match-versus">vs</div>
+    </div>
+
+    <div className="scoreboard-pre-match scoreboard-page">
+      <div className="pre-match-timer"></div>
+      <div className="pre-match-player-name home"></div>
+      <div className="pre-match-player-name away"></div>
+    </div>
+
+    <div className="scoreboard-post-match scoreboard-page">
+      <h1>Post match.</h1>
+    </div>
+
+    <div className="scoreboard-no-match scoreboard-page">
+      <h1>No match.</h1>
+    </div>
+  </div>,
   document.getElementById('scoreboard')
 )
