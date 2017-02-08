85ab95ee8e61ee993f24674443497b273c81c14a
Jared Norman <jared@creepywizard.com>
2017-02-07 18:08:20 -0800

    Add player activation route

--- app/controllers/api/v2/active_players_controller.rb
+++ app/controllers/api/v2/active_players_controller.rb
@@ -2,8 +2,14 @@ module Api
   module V2
     class ActivePlayersController < ApplicationController
       before_action :load_table
+      before_action :load_player, only: :create
 
       def index
+        render json: @table.active_players
+      end
+
+      def create
+        @player.update!(active: true)
         render json: @table.active_players
       end
 
@@ -12,6 +18,13 @@ module Api
       def load_table
         @table = Table.find(params[:table_id])
         authorize! :read, @table
+      rescue ActiveRecord::RecordNotFound
+        render json: nil, status: :not_found
+      end
+
+      def load_player
+        @player = Player.find(params[:active_player][:id])
+        authorize! :update, @table
       rescue ActiveRecord::RecordNotFound
         render json: nil, status: :not_found
       end
--- spec/controllers/api/v2/active_players_controller_spec.rb
+++ spec/controllers/api/v2/active_players_controller_spec.rb
@@ -21,6 +21,44 @@ RSpec.describe Api::V2::ActivePlayersController do
     end
   end
 
-  describe "POST #create"
+  describe "POST #create" do
+    subject do
+      post :create,
+           params: { table_id: table_id, active_player: active_player_params }
+    end
+
+    let(:active_player_params) { { id: player_id } }
+
+    before { sign_in create(:admin_player) }
+
+    context "when the table exists" do
+      let(:table_id) { table.to_param }
+      let(:table) { create :default_table }
+
+      context "when the player exists" do
+        let(:player_id) { player.id }
+        let(:player) { create :player }
+
+        it { is_expected.to have_http_status :ok }
+
+        it "activates the player" do
+          expect { subject }.to change { player.reload.active? }.from(false).to(true)
+        end
+      end
+
+      context "when the player doesn't exist" do
+        let(:player_id) { "space-dog-laika" }
+        it { is_expected.to have_http_status :not_found }
+      end
+    end
+
+    context "when the table doesn't exist" do
+      let(:table_id) { "potato" }
+      let(:player_id) { 1 }
+
+      it { is_expected.to have_http_status :not_found }
+    end
+  end
+
   describe "DELETE #destroy"
 end
