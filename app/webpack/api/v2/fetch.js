794824e97177ae90523b7ca03eae20896acb1f2e
Jared Norman <jared@creepywizard.com>
2017-02-07 19:39:51 -0800

    Correct Content-Type for API requests

--- app/webpack/api/v2/fetch.js
+++ app/webpack/api/v2/fetch.js
@@ -3,7 +3,10 @@ import { token } from '../../utils/csrf'
 export default (path, options = {}) => {
   const defaultOptions = {
     credentials: 'same-origin',
-    headers: { 'X-CSRF-Token': token() }
+    headers: {
+      'X-CSRF-Token': token(),
+      'Content-Type': 'application/json'
+    }
   }
 
   const jsonPromise =
