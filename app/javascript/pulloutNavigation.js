07dc3d49f58591c4ac3c86be1f1d59254e13aa63
Jared Norman <jared@creepywizard.com>
2017-03-28 23:03:17 -0700

    Theoretically toggle pullout navigation

--- app/javascript/pulloutNavigation.js
+++ app/javascript/pulloutNavigation.js
@@ -1,14 +1,21 @@
 import { Observable } from 'rxjs/Observable'
 import { map } from 'rxjs/operator/map'
-
+import { scan } from 'rxjs/operator/scan'
 
 $(() => {
+  const pulloutSelector = '[data-js-pullout-navigation]';
+  const toggleSelector = '[data-js-pullout-navigation-toggle]';
+
   const toggles = Observable.create(observer => {
     $(document.body).on(
       'click',
-      '[data-js-pullout-navigation-toggle]',
+      toggleSelector,
       (e) => observer.next(e)
     )
   })
-  toggles.subscribe(::console.log)
+
+  const showPullout = toggles::scan((acc) => !acc, false)
+
+  showPullout.subscribe((isShowing) =>
+    $(pulloutSelector).toggleClass('active', isShowing))
 })
