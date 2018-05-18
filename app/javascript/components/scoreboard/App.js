f25d58c66e00c8f73bbe29507c22f6b939e64258
Jared Norman <jared@super.gd>
2018-05-18 11:00:39 -0700

    Display warning when connection lost

--- app/javascript/components/scoreboard/App.js
+++ app/javascript/components/scoreboard/App.js
@@ -6,14 +6,35 @@ import TableControls from './TableControls'
 import { tableId } from '../../selectors/table'
 
 const mapStateToProps = (state) => ({
-  tableId: tableId(state)
+  tableId: tableId(state),
+  connected: state.connection.connected
 });
 
-const App = ({ tableId }) => (
-  <div>
-    <Scoreboard/>
-    <TableControls tableId={tableId}/>
+const NoConnection = () => (
+  <div style={{
+    backgroundColor: "#ff2461",
+    position: "relative",
+    width: 1920,
+    height: 1080
+  }}>
+    <h1 style={{
+      fontSize: 250,
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      color: "#ffffff",
+      textAlign: "center"
+    }}>CONNECTION BORKED</h1>
   </div>
 )
+
+const App = ({ tableId, connected }) => {
+  const contents = connected ? [<Scoreboard/>, <TableControls tableId={tableId}/>] : <NoConnection />
+
+  return (
+    <div>{contents}</div>
+  )
+}
 
 export default connect(mapStateToProps)(App)
